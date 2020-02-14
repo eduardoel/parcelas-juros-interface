acca33d6579f01f43f836e38f4b4bea388dd1d00
Eduardo Elly <eduardo_elly@hotmail.com>
2020-02-14 14:58:54 -0300

    Completo

--- src/model/services/OnlinePaymentService.java
+++ src/model/services/OnlinePaymentService.java
@@ -1,7 +1,8 @@
 package model.services;
 //Interface para serviços de pagamento online
+
 public interface OnlinePaymentService {
-    
-    
-    
+
+    double paymentFee(double amount);
+    double interest(double amount, int months);
 }
--- src/parcelasjurosinterface/ParcelasJurosInterface.java
+++ src/parcelasjurosinterface/ParcelasJurosInterface.java
@@ -1,11 +1,45 @@
 package parcelasjurosinterface;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+import model.entities.Contract;
+import model.services.ContractService;
+import model.services.PaypalService;
+
 public class ParcelasJurosInterface {
 
-    public static void main(String[] args) {
-        
-        
-        
+    public static void main(String[] args) throws ParseException {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.println("Enter contract data"); //Inserir dados do contrato
+        System.out.print("Number: ");
+        int number = sc.nextInt();
+        System.out.print("Date (dd/MM/yyyy): ");
+        Date date = sdf.parse(sc.next());
+        System.out.print("Contract value: "); //Valor do contrato
+        double totalValue = sc.nextDouble();
+
+        Contract contract = new Contract(number, date, totalValue);
+
+        System.out.print("Enter number of installments: "); //Digite o número de parcelas
+        int n = sc.nextInt();
+
+        ContractService contractService = new ContractService(new PaypalService());
+
+        contractService.processContrat(contract, n);
+
+        System.out.println("Installments:"); //Parcelas
+        contract.getInstallments().forEach((x) -> {
+            System.out.println(x);
+        });
+
+        sc.close();
     }
-    
+
 }
